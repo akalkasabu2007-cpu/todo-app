9feccbaa0b9e58d6b8090c2b0b807c110e8bc930
Akalka <akalkasabu2007@gmail.com>
2026-06-17 16:00:41 +0530

    todo

---
+++ todo/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class todo(models.Model):
+    taskname=models.CharField(max_length=100)
+    taskdate=models.DateField()
+
+# Create your models here.
---
+++ todo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from .models import todo
+def index(request):
+    item=todo.objects.all()
+    if request.method=='POST':
+        a=request.POST.get('taskname')
+        b=request.POST.get('taskdate')
+        todo.objects.create(taskname=a, taskdate=b)
+        return redirect('home')
+    return render(request,'index.html',{'item':item})
+
+def deletetask(request,id):
+    a=todo.objects.filter(id=id)
+    a.delete()
+    return redirect('home')
+
+def updatetask(request,id):
+    a=todo.objects.get(id=id)
+    if request.method=='POST':
+        a.taskname=request.POST.get('taskname')
+        a.taskdate=request.POST.get('taskdate')
+        a.save()
+        return redirect('home')
+    return render(request,'updates.html', {'a':a})
